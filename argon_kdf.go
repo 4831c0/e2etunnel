89f13faffe52c047e72e49ff792af83c390c3904
Armand <4831c0@proton.me>
2025-09-20 10:04:19 +0200

    Adjust argon parameters

--- argon_kdf.go
+++ argon_kdf.go
@@ -3,9 +3,9 @@ package main
 import "golang.org/x/crypto/argon2"
 
 func deriveArgon2id32FromPSK(psk, salt []byte) []byte {
-	time := uint32(3)
-	memory := uint32(64 * 1024)
-	threads := uint8(4)
+	time := uint32(1)
+	memory := uint32(4 * 1024)
+	threads := uint8(2)
 	keyLen := uint32(32)
 	return argon2.IDKey(psk, salt, time, memory, threads, keyLen)
 }
